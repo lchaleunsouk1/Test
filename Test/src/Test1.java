79a964c369f262a6b4901f690d825af4968ade7d
Leena Chaleunsouk <lchaleunsouk1@student.gsu.edu>
2016-12-03 16:44:46 -0500

    Second try.

--- Test/src/Test1.java
+++ Test/src/Test1.java
@@ -3,6 +3,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		System.out.print("This is the first test by Leena Chaleunsouk.");
+		System.out.print("This is the second push.");
 	}
 
 }
